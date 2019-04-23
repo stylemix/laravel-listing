faa64e72a1f78253b642ec0e5f0025c7daad5886
Azamat <azamat@stylemix.net>
2019-04-23 10:00:29 +0500

    Enum attribute can take options from enum class.

--- src/Attribute/Enum.php
+++ src/Attribute/Enum.php
@@ -2,16 +2,33 @@
 
 namespace Stylemix\Listing\Attribute;
 
+use Illuminate\Support\Arr;
+
 /**
  * @property mixed $source Enum keywords source
  */
 class Enum extends Keyword
 {
 
+	/**
+	 * Enum constructor.
+	 *
+	 * @param string $name
+	 * @param string $enumClass
+	 */
+	public function __construct($name, $enumClass = null)
+	{
+		parent::__construct($name);
+
+		if ($enumClass) {
+			$this->source = $enumClass::choices();
+		}
+	}
+
 	public function applyIndexData($data, $model)
 	{
 		$value = $data->get($this->name);
-		$data[$this->name . '_text'] = $value ? array_get($this->source, $value) : null;
+		$data[$this->name . '_text'] = $value ? Arr::get($this->source, $value) : null;
 	}
 
 	public function getSelectOptions()
